4325efab9603dc2da3f88be233efeeb05090671b
berdibek-2002 <uralov0212@gmail.com>
2024-05-02 09:37:55 +0500

    version 1.0.1

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+  const addTodo = () => {
+    if (input.trim() !== '') {
+      setTodos([...todos, input]);
+      setInput('');
+    }
+  };
+
+  return (
+    <div className='App'>
+      <h1>Todo App</h1>
+      <input
+        type='text'
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder='Enter a todo'
+      />
+      <button onClick={addTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
